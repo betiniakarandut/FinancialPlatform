e303297cbe0a00be42edb5197c0fb029fab36711
betiniakarandut <betiniakarandut@gmail.com>
2024-04-13 04:51:45 +0100

    chore: Database config

--- utils/configuration.js
+++ utils/configuration.js
@@ -7,5 +7,4 @@ const dbPassword = process.env.DB_PASSWORD;
 
 const encodedPassword = encodeURIComponent(dbPassword)
 
-
-export const dbConnStr = `mongodb+srv://${process.env.DB_USERNAME}:${encodedPassword}@cluster0.z3afn56.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+export const dbConnStr = `mongodb+srv://${dbUsername}:${encodedPassword}@cluster0.k7eed4z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
