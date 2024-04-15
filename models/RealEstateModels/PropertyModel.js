409879eec7730efed135ce1df59a57ef6055cc51
betiniakarandut <betiniakarandut@gmail.com>
2024-04-16 00:21:09 +0100

    feat: property model

---
+++ models/RealEstateModels/PropertyModel.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const propertyModelSchema = new mongoose.Schema({
+    name: String,
+    label: {enum: ['For Sale'], default: 'For Sale'},
+    owner: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    amount: Number,
+    address: String,
+    picture: String,
+    createdAt: {type: Date, default: Date.now()},
+});
+
+const Property = mongoose.model("Property", propertyModelSchema);
+export default Property;
