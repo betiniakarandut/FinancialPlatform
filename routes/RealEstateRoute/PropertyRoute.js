e50a2a92d899696d8dbd66cca39c2d1c489ccb16
betiniakarandut <betiniakarandut@gmail.com>
2024-04-18 10:09:14 +0100

    feat: search property route

--- routes/RealEstateRoute/PropertyRoute.js
+++ routes/RealEstateRoute/PropertyRoute.js
@@ -6,6 +6,7 @@ import {
     updateProperty, 
     uploadProperty 
 } from "../../controllers/RealEstateController/PropertyController.js";
+import { searchProperty } from "../../controllers/RealEstateController/SearchProperty.js";
 
 const propertyRoute = express.Router();
 
@@ -23,5 +24,6 @@ const upload = multer({ storage: storage });
 propertyRoute.post('/uploads', upload.single('file'), middlewareAuth, uploadProperty);
 propertyRoute.put('/:propertyId/update', middlewareAuth, updateProperty);
 propertyRoute.delete('/:propertyId/delete', middlewareAuth, deleteProperty);
+propertyRoute.get('/search', searchProperty);
 
 export default propertyRoute;
