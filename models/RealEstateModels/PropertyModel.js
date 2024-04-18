18d69d5832ff7680bd2adf494d38dc8fa6343d59
betiniakarandut <betiniakarandut@gmail.com>
2024-04-18 05:18:37 +0100

    chore: Amount and label

--- models/RealEstateModels/PropertyModel.js
+++ models/RealEstateModels/PropertyModel.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const propertyModelSchema = new mongoose.Schema({
     name: String,
-    label: {enum: ['For Sale'], default: 'For Sale'},
     owner: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    amount: Number,
+    amount: String,
+    label: String,
     address: String,
     picture: String,
     description: String,
